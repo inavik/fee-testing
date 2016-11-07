b928c3e36be5db79afcdbadef5498a9747b86004
Navi Kaur <nkaur901@gmail.com>
2016-11-07 15:29:24 -0500

    created test to add numbers in the array and return the total

--- test/specs/calc.spec.js
+++ test/specs/calc.spec.js
@@ -9,7 +9,22 @@
       it('should know that five equals five', function() {
         expect(5).to.equal(5);
       });
+
+      describe('sum', function() {
+        it('should add the numbers in the array and return six', function() {
+          var result= window.calc.sum([1,2,3]);
+          expect(result).to.equal(6);
+        })
+      });
+
+
+
+
+
+
     });
+
+
 
 
 
